a2b7af0e0440a8e9743b6664dc585742ba7ac786
ono-s3 <ono.s.3mail@gmail.com>
2021-02-03 20:35:05 +0900

    商品削除機能変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,7 @@ class ItemsController < ApplicationController
       @item.destroy
       redirect_to action: :index
     else
-      redirect_to action: :show
+      render :show
     end
   end
 
