8ab81328df4dac85ffa532c0c7b18040bbb55d87
ono-s3 <ono.s.3mail@gmail.com>
2021-02-03 18:45:31 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,8 +20,23 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-    
 
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user.id
+      redirect_to root_path
+    end
+
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
 
 
 
