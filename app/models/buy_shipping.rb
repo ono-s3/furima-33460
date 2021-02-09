26f2f79edd584043afba047bb9932df9bbfcf32b
ono-s3 <ono.s.3mail@gmail.com>
2021-02-09 19:08:07 +0900

    商品購入機能変更

--- app/models/buy_shipping.rb
+++ app/models/buy_shipping.rb
@@ -6,7 +6,7 @@ class BuyShipping
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1, only_integer: true }
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+    validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :token
